f993b9702cc4434f7a26bbec6d9f22e6f835ed13
mogahed0118 <39539716+mogahed0118@users.noreply.github.com>
2021-02-14 16:27:18 +0300

    new

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,73 @@ class _loginScreenState extends State<LoginScreen> {
   // Color color=new Color(hexToInt("FFB74093"));
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-          backgroundColor: Color.fromARGB(30, 32, 0, 75),
-          body: Column(
-            children: [],
-          )),
-    );
+    return Center(
+        child: Stack(
+      alignment: Alignment.topCenter,
+      //   backgroundColor: Color.fromRGBO(133, 36, 227, 1),
+      children: [
+        Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.only(top: 00, left: 00, right: 00),
+            height: 200,
+            width: 900,
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(133, 36, 227, 1),
+              borderRadius: BorderRadius.circular(20), //border corner radius
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black38.withOpacity(0.2), //color of shadow
+                  spreadRadius: 5, //spread radius
+                  blurRadius: 7, // blur radius
+                  offset: Offset(0, 2), // changes position of shadow
+                  //first paramerter of offset is left-right
+                  //second parameter is top to down
+                ),
+                //you can set more BoxShadow() here
+              ],
+            ),
+            child: Column(
+              children: [
+                Padding(padding: EdgeInsets.only(left: 66, top: 66)),
+                Center(
+                    child: Column(
+                  children: [
+                    Text(
+                      "Login",
+                      style: TextStyle(fontSize: 70, color: Colors.white),
+                    ),
+                    // Padding(padding: EdgeInsets.only(left: 66, top: 44)),
+                  ],
+                )),
+              ],
+            )),
+        Padding(padding: EdgeInsets.only(left: 22, top: 00)),
+        CircleAvatar(
+          radius: 50,
+          backgroundColor: Color.fromRGBO(133, 36, 227, 1),
+          child: Container(
+            alignment: Alignment.topCenter,
+            margin: EdgeInsets.only(top: 00, left: 00, right: 00),
+            // height: 200,
+            //width: 900,
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(133, 36, 227, 1),
+              borderRadius: BorderRadius.circular(100), //border corner radius
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black38.withOpacity(0.2), //color of shadow
+                  spreadRadius: 5, //spread radius
+                  blurRadius: 7, // blur radius
+                  offset: Offset(0, 2), // changes position of shadow
+                  //first paramerter of offset is left-right
+                  //second parameter is top to down
+                ),
+                //you can set more BoxShadow() here
+              ],
+            ),
+          ),
+        )
+      ],
+    ));
   }
 }
